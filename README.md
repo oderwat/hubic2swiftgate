35f6e4641306d9c79838cb8e298f6e767a45fb19
Hans Raaf <hara@oderwat.de>
2014-02-13 17:22:31 +0100

    Initial commit!

---
+++ README.md
@@ -0,0 +1,74 @@
+# This is a HubiC to OpenStack Swift Gateway (hubic2swiftgate)
+
+It supports OAuth2 with api.hubic.com and has to be installed on an apache2 webserver using a ssl-certificate.
+
+## Warning!
+
+This tool is not supported by OVH.
+
+## What can it do?
+
+While using a real (non self-signed) certificate I got 100% compabillity with:
+
+* Duplicity swift:// backend
+* swift command line client
+* CyberDuck
+* ExpanDrive2 (this really needs a non self-signed certificate!)
+* Dulwich (GIT Server!) with Swift-Repo Backend by https://github.com/enovance/
+
+I publish this code in an alpha "proof of concept" state. Please don't expect it to be easy to install (but it actually is easy to use after you got it working).
+
+I made it because the HubicSwiftGateway (https://github.com/Toorop/HubicSwiftGateway) "suddenly" stopped working because it's (reverse engineered) authentication scheme is now deprecated.
+
+## How to make it work?
+
+I suppose you have the code run in an apache2 server with php, curl, mod_rewrite, mod_ssl.
+
+The docroot of a virtual server is pointing at the root of this project and asume the server is available under https://yourserver.com/ for this description.
+
+In the current state the gateway only works for one HubiC Client which needs to be registered in the HubiC account panel (developer section).
+
+### Setting things up in you HubiC account:
+
+* Log into your HubiC account
+* Click onto account details
+* Add a new personal client app in the developer section
+* You can choose any name you like (e.g. SwiftGate)
+* Redirect URL needs to point to https://yourserver.com/callback/ (currently https://yourserver.com/ works too)
+
+### Configuring the gateway:
+
+* rename config.php.sample to config.php and put your Client ID and Client Secret in there.
+* change permission on "cache" folder to allow apache to store data there
+* make sure the .htaccess is used or configure your virtual server similar
+
+**To register the gateway with hubic visit:**
+
+https://yourserver.com/register/
+
+If everything is working you are redirected to the Hubic Client Authentication Site. There you need to login with the data of your HubiC account.
+
+After that you get redirected back to your server which should take the code and redirect again to a "success" page.
+
+To verify that it worked you can access: https://yourserver.com/usage/ which should show you a brief space usage report.
+
+I am using the client id and secret for "pseudo" authentication only with the swift clients at the swift gateway.
+
+The real authentication is done with the OAuth2 tokens from the Hubic API "in the background" which then gets the OpenStack Tokens for the API to the filestorage.
+
+###  Configuring the Swift Clients
+
+To use it with "any" client supporting openstack swift protocol you need to set those up similiar to this:
+
+<pre>
+# HubiC for swift and duplicity
+export ST_AUTH='https://yourserver.com/auth/v1.0/'
+export ST_USER='api_hubic_xxx...'
+export ST_KEY='xxx...'
+
+export SWIFT_AUTHURL=$ST_AUTH
+export SWIFT_USERNAME=$ST_USER
+export SWIFT_PASSWORD=$ST_KEY
+</pre>
+
+P.S.: This work is dedicated to my friends from METATEXX GmbH!
---
+++ www/simple.php
@@ -0,0 +1,340 @@
+<?php
+/*
+    Copyright by Hans Raaf (aka OderWat) http://oderwat.de/ && https://github.com/oderwat
+
+    Losely based on work by Stéphane Depierrepont (aka Toorop) toorop@toorop.fr
+    and by Vincent Giersch : https://github.com/gierschv
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy of
+    the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations under
+    the License.
+
+ */
+
+define('CACHEPATH',dirname(__FILE__).'/../cache');
+define('CONFIGFILE',dirname(__FILE__).'/../config.php');
+
+include(CONFIGFILE);
+
+/*
+function logfile($txt) {
+	if(!is_string($txt)) {
+		ob_start();
+		print_r($txt);
+		$txt=ob_get_contents();
+		ob_end_clean();
+	}
+	$fh=fopen(CACHEPATH.'/swiftgateway.log','a');
+	fputs($fh,$txt);
+	fputs($fh,"\n");
+	fclose($fh);
+}
+*/
+
+function internal_error($txt){
+	header("HTTP/1.0 500 Internal Server Error");
+	if($txt) {
+		print($txt);
+	}
+	die();
+}
+
+function format_bytes($bytes, $precision = 2) {
+	$units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+	$bytes = max($bytes, 0);
+	$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+	$pow = min($pow, count($units) - 1);
+
+	// Uncomment one of the following alternatives
+	// $bytes /= pow(1024, $pow);
+	$bytes /= (1 << (10 * $pow));
+
+	return round($bytes, $precision) . ' ' . $units[$pow];
+}
+
+// Curl is available ?
+if(!function_exists('curl_init')) {
+	internal_error('Curl seems to be not supported by your PHP version, checks : http://php.net/manual/en/curl.installation.php');
+}
+
+// Cache is ok ?
+if(!file_exists(CACHEPATH)) {
+	internal_error(CACHEPATH .' doesn\'t exists');
+}
+if(!is_writable(CACHEPATH)) {
+	internal_error(CACHEPATH .' is not writable');
+}
+
+$redirect_uri="https://".$_SERVER['SERVER_NAME']."/";
+
+$basic_auth=base64_encode($client_id.':'.$client_secret);
+
+$cacheKey=$client_id;
+
+$mode=false;
+
+list($request)=explode('?',$_SERVER['REQUEST_URI']);
+
+switch($request) {
+	case '/v1.0':
+	case '/v1.0/':
+	case '/auth':
+	case '/auth/':
+	case '/auth/v1.0':
+	case '/auth/v1.0/':
+		$mode='swift';
+		break;
+
+	case '/register':
+	case '/register/':
+		$mode='register';
+		break;
+
+	case '/usage':
+	case '/usage/':
+		$mode='usage';
+		break;
+
+	case '/':
+		if(isset($_GET['code'])) {
+			$mode='callback';
+		}
+		break;
+
+	case '/callback':
+	case '/callback/':
+		$mode='callback';
+		break;
+
+	case '/success':
+	case '/success/':
+		$mode='success';
+		break;
+
+}
+
+if(!$mode) {
+	header('HTTP/1.0 404 Not Found');
+	print("Not Found!");
+	die();
+}
+
+if($mode=='swift') {
+	// Get Auth from Headers to artificially limit access!
+	if (!isset($_SERVER['HTTP_X_AUTH_USER']) || !isset($_SERVER['HTTP_X_AUTH_KEY'])) {
+		header("HTTP/1.0 403 Forbidden");
+		print('AUTH_USER and/or AUTH_KEY are missing!');
+		die();
+	} else {
+		$auth_user = $_SERVER['HTTP_X_AUTH_USER'];
+		$auth_key = $_SERVER['HTTP_X_AUTH_KEY'];
+	}
+
+	if($auth_user!=$client_id || $auth_key!=$client_secret) {
+		header('HTTP/1.0 403 Access Denied');
+		print("ERROR : Access Denied");
+		die();
+	}
+}
+
+if($mode=='success') {
+	header('HTTP/1.0 200 OK');
+	print("Success: This Server is now registered with HubiC Filestorage!");
+	die();
+}
+
+// User auf die Api Register Seite schicken
+if($mode=='register') {
+	$uri='https://api.hubic.com/oauth/auth/?';
+	$uri.='client_id='.$client_id;
+	$uri.='&redirect_uri='.urlencode($redirect_uri);
+	$uri.='&scope=usage.r,account.r,getAllLinks.r,credentials.r,activate.w,links.drw';
+	$uri.='&response_type=code';
+	$uri.='&state='.$client_id.':123qweasd';
+	header('HTTP/1.0 301 Redirect');
+	header("Location: ".$uri);
+	die();
+}
+
+$access_token=false;
+$access_expires=0;
+$refresh_token=false;
+
+if($mode=='callback') {
+	// we return from OAuth2 on HubiC Site
+	if(!isset($_GET['code'])) {
+		header('HTTP/1.0 412 Precondition failed');
+		print("Illegal!");
+		die();
+	}
+	$code=$_GET['code'];
+
+	$c = curl_init('https://api.hubic.com/oauth/token/');
+	curl_setopt($c, CURLOPT_HTTPHEADER, array(
+		'Authorization: Basic '.$basic_auth
+	));
+	curl_setopt($c, CURLOPT_VERBOSE, 0);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+	curl_setopt($c, CURLOPT_POST, true );
+	curl_setopt($c, CURLOPT_POSTFIELDS, array(
+		'code' => $code,
+		'redirect_uri' => $redirect_uri,
+		'grant_type' => 'authorization_code'
+	) );
+
+	$r = curl_exec($c);
+	$http_retcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
+	$error = curl_error($c);
+	if ($http_retcode !== 200) {
+		header('HTTP/1.0 ' . $http_retcode);
+		print("ERROR 99: ".$error);
+		die();
+	}
+
+	$token = json_decode($r);
+
+	if($token->token_type != 'Bearer') {
+		internal_error('Unkknown Token Type: '.$token->token_type);
+	}
+
+	$access_token=$token->access_token;
+	$access_expires=$token->expires_in+time();
+	$refresh_token=$token->refresh_token;
+
+	if (file_exists(CACHEPATH.'/'.$cacheKey)) {
+		// delete outdated cached data
+		unlink(CACHEPATH.'/'.$cacheKey);
+	}
+
+}
+
+if (file_exists(CACHEPATH.'/'.$cacheKey)) {
+	$cached=unserialize(file_get_contents(CACHEPATH.'/'.$cacheKey));
+	if($mode=='swift' && $cached['os_expires']>time()) {
+		// OS Token still valid
+		header('X-Storage-Url: '.$cached['os_endpoint']);
+		header('X-Auth-Token: '.$cached['os_token']);
+		//header('HTTP/1.0 204 No Content');
+		header('HTTP/1.0 200 OK');
+		die();
+	}
+
+	// Preload hubic tokens from cache
+	$access_token=$cached['access_token'];
+	$access_expires=$cached['access_expires'];
+	$refresh_token=$cached['refresh_token'];
+
+}
+
+if(!$access_token || $access_expires<time()) {
+	$c = curl_init('https://api.hubic.com/oauth/token/');
+	curl_setopt($c, CURLOPT_HTTPHEADER, array(
+		'Authorization: Basic '.$basic_auth
+	));
+
+	curl_setopt($c, CURLOPT_VERBOSE, 0);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+	curl_setopt($c, CURLOPT_POST, true );
+	curl_setopt($c, CURLOPT_POSTFIELDS, array(
+		'refresh_token' => $refresh_token,
+		'grant_type' => 'refresh_token'
+	) );
+
+	$r = curl_exec($c);
+	$http_retcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
+	$error = curl_error($c);
+	if ($http_retcode !== 200) {
+		header('HTTP/1.0 ' . $http_retcode);
+		print("ERROR 178: ".$error);
+		die();
+	}
+
+	$token = json_decode($r);
+
+	if($token->token_type != 'Bearer') {
+		internal_error('Unkknown Token Type: '.$token->token_type);
+	}
+
+	$access_token=$token->access_token;
+}
+
+if($mode=='usage') {
+	$c = curl_init('https://api.hubic.com/1.0/account/usage/');
+	curl_setopt($c, CURLOPT_HTTPHEADER, array(
+		'Authorization: Bearer '.$access_token
+	));
+
+	curl_setopt($c, CURLOPT_VERBOSE, 0);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+	$r = curl_exec($c);
+	$http_retcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
+	$error = curl_error($c);
+	if ($http_retcode !== 200) {
+		header('HTTP/1.0 ' . $http_retcode);
+		print("ERROR 207 : ".$error);
+		print_r('access token: '.$access_token);
+		flush();
+		die();
+	}
+
+	$usage = json_decode($r);
+
+	print('<pre>Usage: ');
+	print(format_bytes($usage->used).' / '.format_bytes($usage->quota));
+	die();
+}
+
+// Nun OpenStack Swift Storage Token holen
+$c = curl_init('https://api.hubic.com/1.0/account/credentials');
+curl_setopt($c, CURLOPT_HTTPHEADER, array(
+	'Authorization: Bearer '.$access_token
+));
+
+curl_setopt($c, CURLOPT_VERBOSE, 0);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+$r = curl_exec($c);
+$http_retcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
+$error = curl_error($c);
+if ($http_retcode !== 200) {
+	header('HTTP/1.0 ' . $http_retcode);
+	print("ERROR 207 : ".$error);
+	die();
+}
+
+$storage = json_decode($r);
+
+// put in cache
+file_put_contents(CACHEPATH.'/'.$cacheKey,serialize(
+	array(
+		'os_expires'=>strtotime($storage->expires),
+		'os_endpoint'=>$storage->endpoint,
+		'os_token'=>$storage->token,
+		'access_token'=>$access_token,
+		'access_expires'=>$access_expires,
+		'refresh_token'=>$refresh_token
+	)));
+
+if($mode=='callback') {
+	header('HTTP/1.0 301 Redirect');
+	header('Location: https://'.$_SERVER['HTTP_HOST'].'/success/');
+} else if($mode=='swift') {
+	header('X-Storage-Url: '.$storage->endpoint);
+	header('X-Auth-Token: '.$storage->token);
+	header('HTTP/1.0 204 No Content');
+} else {
+	header('HTTP/1.0 404 Not Found');
+	print("Not Found!");
+}
